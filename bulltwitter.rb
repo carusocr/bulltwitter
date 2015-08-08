87f052c8251a16197af8c00592b81b0c6c654d37
Chris Caruso <carusocr@ldc.upenn.edu>
2015-08-08 15:20:40 -0400

    baby steps with map context menu

--- bulltwitter.rb
+++ bulltwitter.rb
@@ -22,22 +22,15 @@ cfgfile = 'auth.cfg'
 
 cnf = YAML::load(File.open(cfgfile))
 
-#client = Twitter::REST::Client.new do |config|
-#  config.consumer_key       = cnf['crcbot']['con_key']
-#  config.consumer_secret    = cnf['crcbot']['con_sec']
-#  config.access_token        = cnf['crcbot']['o_tok']
-#  config.access_token_secret = cnf['crcbot']['o_tok_sec']
-#end
-
 con_key       = cnf['crcbot']['con_key']
 con_sec    = cnf['crcbot']['con_sec']
 access_token        = cnf['crcbot']['o_tok']
 access_token_secret = cnf['crcbot']['o_tok_sec']
 
-#client.update(status)
-
 #testing straight post
 consumer = OAuth::Consumer.new(con_key,con_sec, :site => "http://api.twitter.com", :scheme => :header)
 access_token = OAuth::AccessToken.from_hash(consumer, :oauth_token => access_token, :oauth_token_secret => access_token_secret)
-#this worked
-access_token.request(:post, "https://api.twitter.com/1.1/statuses/update.json", {"Content-Type" => "application/json", "status"=>"foo"})
+#this worked, returned San Francisco
+access_token.request(:post, "https://api.twitter.com/1.1/statuses/update.json", {"Content-Type" => "application/json", "status"=>"Zurich2", "lat"=>37.78217, "long"=>-122.40062})
+
+#how do I get a placeid given coords? 

aac1ca923df98469f27af71613f4a0cd31416ddf
Chris Caruso <carusocr@ldc.upenn.edu>
2015-08-17 14:49:37 -0400

    got tweet working

--- bulltwitter.rb
+++ bulltwitter.rb
@@ -1,36 +1,64 @@
 #!/usr/bin/env ruby
-=begin
+=begin 
 
-General idea of this app is that I can make a tweet that's tagged to a geolocation
-other than my actual location. Ideally the interface will be a Google Maps view, and I
-can zoom in, click a location, and select a Make Tweet From Here' button to make 
-tweet that displays as being from that lat+long. 
-
-First goal is to generate a tweet with artificial geotagged info. 
-
-Second is to add a map interface.
+Socket.rb will listen for the map page's creation of a websocket. Once it's 
+found it, it will then start listening for events from the bulltwitter map and
+call the tweet script when a message is received.
 
 =end
 
+require 'em-websocket'
+require 'uuid'
+require 'bunny'
 require 'twitter'
 require 'yaml'
 require 'json'
 require 'oauth'
 
 cfgfile = 'auth.cfg'
-#status = ARGV[0]
-
 cnf = YAML::load(File.open(cfgfile))
+$con_key       = cnf['crcbot']['con_key']
+$con_sec    = cnf['crcbot']['con_sec']
+$access_token        = cnf['crcbot']['o_tok']
+$access_token_secret = cnf['crcbot']['o_tok_sec']
 
-con_key       = cnf['crcbot']['con_key']
-con_sec    = cnf['crcbot']['con_sec']
-access_token        = cnf['crcbot']['o_tok']
-access_token_secret = cnf['crcbot']['o_tok_sec']
+def bulltweet(lat,lng,status)
 
-#testing straight post
-consumer = OAuth::Consumer.new(con_key,con_sec, :site => "http://api.twitter.com", :scheme => :header)
-access_token = OAuth::AccessToken.from_hash(consumer, :oauth_token => access_token, :oauth_token_secret => access_token_secret)
-#this worked, returned San Francisco
-access_token.request(:post, "https://api.twitter.com/1.1/statuses/update.json", {"Content-Type" => "application/json", "status"=>"Zurich2", "lat"=>37.78217, "long"=>-122.40062})
+  consumer = OAuth::Consumer.new($con_key,$con_sec, :site => "http://api.twitter.com", :scheme => :header)
+  access_token = OAuth::AccessToken.from_hash(consumer, :oauth_token => $access_token, :oauth_token_secret => $access_token_secret)
+  access_token.request(:post, "https://api.twitter.com/1.1/statuses/update.json", {"Content-Type" => "application/json", "status"=>"#{status}", "lat"=>lat, "long"=>lng})
 
-#how do I get a placeid given coords? 
+end
+
+EM.run {
+  EM::WebSocket.run(:host => "127.0.0.1", :port => 8567) do |ws|
+    ws.onopen do
+      puts "WebSocket opened"
+      conn = Bunny.new
+      conn.start
+      ch = conn.create_channel
+      ch.queue("tweets").subscribe do |delivery_info, properties, body|
+        puts "Received tweet\n"
+        encoded_tweet=body.force_encoding("iso-8859-1").force_encoding("utf-8")
+        puts encoded_tweet
+        ws.send encoded_tweet
+      end
+    end
+    ws.onclose do
+      ws.close(code = nil, body = nil)
+      puts "WebSocket closed"
+      exit
+    end
+    ws.onmessage do |msg|
+      # check for coords in parens + string
+      if msg =~ /\(-?\d{1,3}\.\d+, -?\d{1,3}\.\d+\)/
+        puts "got #{msg}\n"
+        lat,lng,tweet = msg.match(/\((-?\d{1,3}\.\d+), (-?\d{1,3}\.\d+)\)(.+)$/).captures
+        bulltweet(lat,lng,tweet)
+      else
+        puts "Got unintelligible command, please resend."
+      end
+    end
+  end
+}
+
